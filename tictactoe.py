aedbd08f0f282301fd54a836410635a671964067
GaryTin <tinkwaiki@gmail.com>
2022-07-15 17:48:04 +0800

    .

--- tictactoe.py
+++ tictactoe.py
@@ -16,7 +16,7 @@ class TicTacToe():
                 else:
                     print("   ", end="")
             print()
-        print(self.board)
+        #print(self.board)
 
     def choose(self,loc):
         if self.is_A_turn:
@@ -26,20 +26,41 @@ class TicTacToe():
 
         self.is_A_turn = not(self.is_A_turn)
 
+    def is_win(self, chosen_index) -> str:
+        x = chosen_index%3
+        y = chosen_index//3
 
+        if (self.board[3*0+x] == self.board[3*1+x] and self.board[3*0+x] == self.board[3*2+x]) or (self.board[3*y+0] == self.board[3*y+1] and self.board[3*y+0] == self.board[3*y+2]):
+            return self.board[chosen_index]
+        if chosen_index % 2 == 0:
+            #check diagonal
+            if x+y == 2:
+                if self.board[2]== self.board[4] and self.board[2]== self.board[6]:
+                    return self.board[chosen_index]
+            elif x==y:
+                if self.board[0]== self.board[4] and self.board[0]== self.board[8]:
+                    return self.board[chosen_index]
+        return "N"
 
+    def start_game(self):
+        while True:
+            self.draw_game()
 
+            user_input = int(input("Please enter which index you want: "))
+
+            while(self.board[user_input]!= "N"):
+                user_input = int(input("Please enter a valid index: "))
+
+            self.choose(user_input)
+
+            if (self.is_win(user_input) != "N"):
+                self.draw_game()
+                print(self.is_win(user_input) + "Win")
+                break
 
 def main():
     tictactoe = TicTacToe()
-    tictactoe.draw_game()
-
-    tictactoe.choose(0)
-    tictactoe.choose(1)
-    tictactoe.choose(2)
-    tictactoe.choose(4)
-    tictactoe.draw_game()
-
+    tictactoe.start_game()
 
 if __name__ == "__main__":
     main()
